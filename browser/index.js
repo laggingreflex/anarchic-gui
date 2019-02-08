68f3b0a97ee687107037818ac03a605d2666fc72
laggingreflex <laggingreflex@gmail.com>
2019-02-08 13:05:32 +0530

    export hText

--- browser/index.js
+++ browser/index.js
@@ -1,6 +1,8 @@
 const { createElement } = require('react');
 const H = exports.H = require('hyperchain/react');
 const h = exports.h = H();
+const HText = exports.HText = require('hyperchain/text');
+const hText = exports.hText = HText();
 exports.createElement = createElement;
 exports.render = require('./render');
 exports.component = require('./component');
